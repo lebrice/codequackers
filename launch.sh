78cda187a2a6e009ebb9d307de60f9c6cf7e16a2
Fabrice Normandin <fabrice.normandin@gmail.com>
2019-10-12 15:03:28 -0400

    Initial Commit, Starting Pure-Pursuit package
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- launch.sh
+++ launch.sh
@@ -5,3 +5,4 @@ set -e
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
 echo "This is an empty launch script. Update it to launch your application."
+roslaunch ros_repo_fabrice_normandin indefinite_navigation.launch
